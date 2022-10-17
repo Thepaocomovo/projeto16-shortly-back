f9c2166c9d81ee212a14e4e2eec389383e53a92f
wesley <wesleysantos20012001@gmail.com>
2022-10-17 13:30:24 -0300

    fix: database connection

--- src/database/Postgres.js
+++ src/database/Postgres.js
@@ -1,18 +1,17 @@
 import pg from 'pg';
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = '123123';
-const host = 'localhost';
-const port = 5432;
-const database = 'shortly';
-
-const connection = {
+const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
       rejectUnauthorized: false
   }
-};
+});
+
+
 
 export default connection;
